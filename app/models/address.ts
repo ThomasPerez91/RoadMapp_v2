55fdbbbc6f8369728013b3f1e79d0aafc52979ea
ThomasPerez91 <dialldialldiall@gmail.com>
2025-05-08 19:18:35 +0200

    add: addresses model + migration

---
+++ app/models/address.ts
@@ -0,0 +1,36 @@
+import { DateTime } from 'luxon'
+import { BaseModel, belongsTo, column } from '@adonisjs/lucid/orm'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+import User from '#models/user'
+
+export default class Address extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare name: string
+
+  @column()
+  declare address: string
+
+  @column()
+  declare postalCode: string
+
+  @column()
+  declare city: string
+
+  @column()
+  declare userId: number
+
+  @column()
+  declare isHome: boolean
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+
+  @belongsTo(() => User)
+  declare user: BelongsTo<typeof User>
+}
